230a1e84a6830bdc579f87e3235a2c58d4acff5b
LHenrique2502 <120137183+LHenrique2502@users.noreply.github.com>
2024-11-06 11:11:24 -0300

    feat: add transaction type badge

--- app/transactions/_components/type-bdge.tsx
+++ app/transactions/_components/type-bdge.tsx
@@ -17,8 +17,8 @@ const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
   }
   if (transaction.type === TransactionType.EXPENSE) {
     return (
-      <Badge className="text-danger bg-danger hover:bg-danger bg-opacity-10 font-bold hover:bg-opacity-10">
-        <CircleIcon className="mr-2 fill-destructive" size={10} />
+      <Badge className="bg-danger bg-opacity-10 font-bold text-danger hover:bg-danger hover:bg-opacity-10">
+        <CircleIcon className="mr-2 fill-danger" size={10} />
         Despesa
       </Badge>
     );
