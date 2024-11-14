4ba92d9b2ad7ed46349030e4987788d468b9d510
LHenrique2502 <120137183+LHenrique2502@users.noreply.github.com>
2024-11-14 17:51:28 -0300

    feat: add ai report generation

---
+++ app/(home)/_actions/generate-ai-report/index.ts
@@ -0,0 +1,59 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import OpenAI from "openai";
+import { generateAiReportSchema } from "./schema";
+
+export const generateAiReport = async ({ month }: generateAiReportSchema) => {
+  generateAiReportSchema.parse({ month });
+  const { userId } = await auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user.publicMetadata.subscriptionPlan === "premium";
+  if (!hasPremiumPlan) {
+    throw new Error("You need a premium plan to generate AI reports");
+  }
+
+  const openAI = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  // pegar as transações do mês recebido
+  const transactions = await db.transaction.findMany({
+    where: {
+      date: {
+        gte: new Date(`2024-${month}-01`),
+        lt: new Date(`2024-${month}-31`),
+      },
+    },
+  });
+  // mandar as transações para o chatGPT e pedir para gerar um relatório com insights
+
+  const content = `Gere um relatório com insights sobre as minhas finanças, com dicas e orientações de como melhorar minha vida financeira. 
+  As transações estão divididas por ponto e virgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas: 
+  ${transactions
+    .map(
+      (transaction) =>
+        `${transaction.date.toLocaleDateString("pt-BR")}-$-R$${transaction.amount}-${transaction.type}-${transaction.category}`,
+    )
+    .join(";")}`;
+  const completion = await openAI.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content:
+          "Você é um especialista em gestão e organização de finanças pessoais. Você ajuda as pessoas a organizarem melhor as suas finanças.",
+      },
+      {
+        role: "user",
+        content,
+      },
+    ],
+  });
+
+  // pegar o relatório gerado pelo chatGPT e retornar para o usuário
+  return completion.choices[0].message.content;
+};
